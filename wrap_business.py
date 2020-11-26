5ceedfaf16250aa64455d38f84091ce00aa82da5
ben062930 <70728503+ben062930@users.noreply.github.com>
2020-11-26 16:45:11 -0500

    Update wrap_business.py

--- wrap_business.py
+++ wrap_business.py
@@ -1,4 +1,5 @@
 import string
+from pandas import DataFrame
 
 def wrap_business(hw):
     result = list()
@@ -26,15 +27,21 @@ def wrap_business(hw):
     return(name,purpose)
 
 # open files downloaded from the discussion board
-jd = open("/Users/jiefudong/Desktop/Jeff/SIT/FA/FE595/HW3/jiefu dong.txt")
-hl = open("/Users/jiefudong/Desktop/Jeff/SIT/FA/FE595/HW3/han luo.txt")
-zxx = open("/Users/jiefudong/Desktop/Jeff/SIT/FA/FE595/HW3/zhixuan xia.txt")
-tyy = open("/Users/jiefudong/Desktop/Jeff/SIT/FA/FE595/HW3/tianyi yang.txt")
-mgg = open("/Users/jiefudong/Desktop/Jeff/SIT/FA/FE595/HW3/mengge geng.txt")
-content1 = wrap_business(jd)
-content2 = wrap_business(hl)
-content3 = wrap_business(zxx)
-content4 = wrap_business(tyy)
-content5 = wrap_business(mgg)
-business_name = content1[0] + content2[0] + content3[0] + content4[0] + content5[0]
-business_purpose = content1[1] + content2[1] + content3[1] + content4[1] + content5[1]
+business_name = []
+business_purpose = []
+
+docs_ = ["jiefu dong.txt",
+"han luo.txt",
+"zhixuan xia.txt",
+"tianyi yang.txt",
+"mengge geng.txt"]
+print(type(docs_[0]))
+for doc in docs_:
+    temp_content = wrap_business(open(doc))
+    business_name += temp_content[0]
+    business_purpose += temp_content[1]
+
+
+data = {"Business_name":business_name, "Business_purpose":business_purpose}
+df = DataFrame(data)
+# print(df)
